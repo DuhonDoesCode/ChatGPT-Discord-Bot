a6bce5e6c43729ebe53cdaa1119c3ef357317149
DuhonDoesCode <61638618+DuhonDoesCode@users.noreply.github.com>
2022-12-15 18:04:38 -0300

    Initial Commit

---
+++ main_publish.py
@@ -0,0 +1,76 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from typing import Literal, Optional
+from discord.ext.commands import Greedy, Context
+import openai
+import time
+
+class Bot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        super().__init__(command_prefix=commands.when_mentioned_or('$'), intents=intents)
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+bot = Bot()
+
+# Tree syncer
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(
+  ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+openai.organization = "org-ml3jQpNQbsZ9wAD3ZEeDhGX6"
+openai.api_key = "openai_key"
+
+@bot.tree.command()
+async def chat(interaction: discord.Interaction, args: str):
+    await interaction.response.send_message("Wait a bit as I request the response from ChatGPT! (10 seconds)", ephemeral=True)
+    try:
+        completion = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=args,
+            max_tokens=1024,
+            temperature=0.5,
+        )
+        time.sleep(10)
+        await interaction.followup.send(completion.choices[0].text)
+    except:
+        await interaction.followup.send("My OpenAI API key might be overloaded, try again later?")
+    
+
+bot.run('bot_key')
